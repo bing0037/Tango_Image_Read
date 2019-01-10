11c8396e9aaf4ee8515db064f557d43d389162aa
Bingbing <libing0037@gmail.com>
2019-01-10 09:04:20 -0500

    reading tango color image successfully

---
+++ app/src/main/java/org/tensorflow/demo/CameraActivity.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tensorflow.demo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.Image.Plane;
+import android.media.ImageReader;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Trace;
+import android.util.Size;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.widget.Toast;
+import java.nio.ByteBuffer;
+
+import org.tensorflow.demo.env.ImageUtils;
+import org.tensorflow.demo.env.Logger;
+
+public abstract class CameraActivity extends Activity {
+  private static final Logger LOGGER = new Logger();
+
+  private static final int PERMISSIONS_REQUEST = 1;
+
+  private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+  private static final String PERMISSION_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+  private boolean debug = false;
+
+  private Handler handler;
+  private HandlerThread handlerThread;
+  private boolean useCamera2API;
+
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+
+    LOGGER.d("onCreate " + this);
+    super.onCreate(null);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+    setContentView(R.layout.camera_connection_fragment_tracking);
+
+  }
+
+
+  @Override
+  public synchronized void onStart() {
+    LOGGER.d("onStart " + this);
+    super.onStart();
+  }
+
+  @Override
+  public synchronized void onResume() {
+    LOGGER.d("onResume " + this);
+    super.onResume();
+
+    handlerThread = new HandlerThread("inference");
+    handlerThread.start();
+    handler = new Handler(handlerThread.getLooper());
+  }
+
+  @Override
+  public synchronized void onPause() {
+    LOGGER.d("onPause " + this);
+
+    handlerThread.quitSafely();
+    try {
+      handlerThread.join();
+      handlerThread = null;
+      handler = null;
+    } catch (final InterruptedException e) {
+      LOGGER.e(e, "Exception!");
+    }
+
+    super.onPause();
+  }
+
+  @Override
+  public synchronized void onStop() {
+    LOGGER.d("onStop " + this);
+    super.onStop();
+  }
+
+  @Override
+  public synchronized void onDestroy() {
+    LOGGER.d("onDestroy " + this);
+    super.onDestroy();
+  }
+
+  protected synchronized void runInBackground(final Runnable r) {
+    if (handler != null) {
+      handler.post(r);
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(
+          final int requestCode, final String[] permissions, final int[] grantResults) {
+    switch (requestCode) {
+      case PERMISSIONS_REQUEST: {
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+          //setFragment();
+        } else {
+          requestPermission();
+        }
+      }
+    }
+  }
+
+  private boolean hasPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED &&
+              checkSelfPermission(PERMISSION_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    } else {
+      return true;
+    }
+  }
+
+  private void requestPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA) ||
+              shouldShowRequestPermissionRationale(PERMISSION_STORAGE)) {
+        Toast.makeText(CameraActivity.this,
+                "Camera AND storage permission are required for this demo", Toast.LENGTH_LONG).show();
+      }
+      requestPermissions(new String[] {PERMISSION_CAMERA, PERMISSION_STORAGE}, PERMISSIONS_REQUEST);
+    }
+  }
+
+  // Returns true if the device supports the required hardware level, or better.
+  boolean isHardwareLevelSupported(CameraCharacteristics characteristics, int requiredLevel) {
+    int deviceLevel = characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL);
+    if (deviceLevel == CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY) {
+      return requiredLevel == deviceLevel;
+    }
+    // deviceLevel is not LEGACY, can use numerical sort
+    return requiredLevel <= deviceLevel;
+  }
+
+  private String chooseCamera() {
+    final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+    try {
+      for (final String cameraId : manager.getCameraIdList()) {
+        final CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+
+        // We don't use a front facing camera in this sample.
+        final Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
+        if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
+          continue;
+        }
+
+        final StreamConfigurationMap map =
+                characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+        if (map == null) {
+          continue;
+        }
+
+        useCamera2API = isHardwareLevelSupported(characteristics,
+                CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_FULL);
+        LOGGER.i("Camera API lv2?: %s", useCamera2API);
+        return cameraId;
+      }
+    } catch (CameraAccessException e) {
+      LOGGER.e(e, "Not allowed to access camera");
+    }
+
+    return null;
+  }
+
+  public boolean isDebug() {
+    return debug;
+  }
+
+
+}
--- app/src/main/java/org/tensorflow/demo/MainActivity.java
+++ app/src/main/java/org/tensorflow/demo/MainActivity.java
@@ -1,13 +1,412 @@
 package org.tensorflow.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.media.ImageReader;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.media.Image;
+import android.media.Image.Plane;
+import android.media.ImageReader;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.os.Trace;
+import android.util.Log;
+import android.util.Size;
+import android.util.TypedValue;
+import android.view.Display;
+import android.widget.Toast;
+import android.view.View;
+import android.speech.tts.TextToSpeech;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.Tango.OnTangoUpdateListener;
+import com.google.atap.tangoservice.Tango.TangoUpdateCallback;
+import com.google.atap.tangoservice.TangoCameraIntrinsics;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoInvalidException;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPointCloudData;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
+import com.google.atap.tangoservice.experimental.TangoImageBuffer;
+import com.google.tango.depthinterpolation.TangoDepthInterpolation;
+import com.google.tango.support.TangoPointCloudManager;
+import com.google.tango.support.TangoSupport;
+import com.google.tango.transformhelpers.TangoTransformHelper;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import java.util.concurrent.TimeUnit;
+import java.lang.Math;
+import java.util.Locale;
+
+//import org.tensorflow.demo.OverlayView.DrawCallback;
+import org.tensorflow.demo.env.BorderedText;
+import org.tensorflow.demo.env.ImageUtils;
+import org.tensorflow.demo.env.Logger;
+import org.tensorflow.demo.R;
+
+/**
+ * Created by manjekarbudhai on 7/27/17.
+ */
+
+public class MainActivity extends CameraActivity  {
+
+    private Tango tango_;
+    private TangoConfig tangoConfig_;
+    private volatile boolean tangoConnected_ = false;
+    private TangoPointCloudManager mPointCloudManager;
+    HashMap<Integer, Integer> cameraTextures_ = null;
+    private GLSurfaceView view_;
+    private Renderer renderer_;
+    private volatile TangoImageBuffer mCurrentImageBuffer;
+    private int mDisplayRotation = 0;
+    private Matrix rgbImageToDepthImage;
+    TextToSpeech tts1;
+
+
+    private class MeasuredPoint {
+        public double mTimestamp;
+        public float[] mDepthTPoint;
+
+        public MeasuredPoint(double timestamp, float[] depthTPoint) {
+            mTimestamp = timestamp;
+            mDepthTPoint = depthTPoint;
+        }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
+        // GLSurfaceView for RGB color camera
+
+        tts1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+
+        rgbImageToDepthImage = ImageUtils.getTransformationMatrix(
+                640, 480,
+                1920, 1080,
+                0, true);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        cameraTextures_ = new HashMap<>();
+        mPointCloudManager = new TangoPointCloudManager();
+
+        // Request depth in the Tango config because otherwise frames
+        // are not delivered.
+        tango_ = new Tango(this, new Runnable(){
+            @Override
+            public void run(){
+                synchronized (this) {
+                    try {
+                        tangoConfig_ = tango_.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+                        tangoConfig_.putBoolean(TangoConfig.KEY_BOOLEAN_COLORCAMERA, true);
+                        tangoConfig_.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true);
+                        tangoConfig_.putInt(TangoConfig.KEY_INT_DEPTH_MODE, TangoConfig.TANGO_DEPTH_MODE_POINT_CLOUD);
+                        tango_.connect(tangoConfig_);
+                        startTango();
+                        TangoSupport.initialize(tango_);
+                        //cameraTextures_ = new HashMap<>();
+
+                    } catch (TangoOutOfDateException e) {
+                        Log.i("new Tango", "error in onCreate");
+                    }
+                }
+            }
+        });
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        cameraTextures_ = new HashMap<>();
+        view_ = (GLSurfaceView)findViewById(R.id.surfaceviewclass);
+        view_.setEGLContextClientVersion(2);
+        view_.setDebugFlags(GLSurfaceView.DEBUG_CHECK_GL_ERROR);
+        view_.setRenderer(renderer_ = new Renderer(this));
+        view_.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+        new Thread(new RunDetection()).start();
+
+    }
+
+    @Override
+    public void onStart(){
+        Log.i("onStart " , "Main onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i("onResume ", "Main onResume called");
+        super.onResume();
+        //startTango();
+        if (tango_ == null) {
+            startActivityForResult(
+                    Tango.getRequestPermissionIntent(Tango.PERMISSIONTYPE_MOTION_TRACKING),
+                    Tango.TANGO_INTENT_ACTIVITYCODE);
+
+
+            tango_ = new Tango(this, new Runnable() {
+
+                @Override
+                public void run() {
+                    Log.i("onResume ", "new tango");
+                    synchronized (this) {
+                        try {
+                            tangoConfig_ = tango_.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+                            tangoConfig_.putBoolean(TangoConfig.KEY_BOOLEAN_COLORCAMERA, true);
+                            tangoConfig_.putBoolean(TangoConfig.KEY_BOOLEAN_AUTORECOVERY, true);
+                            tango_.connect(tangoConfig_);
+                            startTango();
+                            TangoSupport.initialize(tango_);
+                            //cameraTextures_ = new HashMap<>();
+
+                        } catch (TangoOutOfDateException e) {
+                            Log.i("new Tango", "error in onCreate");
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        /*if(tts1 !=null){
+            tts1.stop();
+            tts1.shutdown();
+        }*/
+        synchronized (this) {
+            try {
+                if (tango_ != null) {
+                    tango_.disconnect();
+                    tangoConnected_ = false;
+                }
+            }
+            catch (TangoErrorException e) {
+                Toast.makeText(
+                        this,
+                        "Tango error: " + e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public synchronized void onStop() {
+        Log.i("onStop " , "Main onStop");
+        super.onStop();
+    }
+
+    public synchronized void attachTexture(final int cameraId, final int textureName) {
+        if (textureName > 0) {
+            // Link the texture with Tango if the texture changes after
+            // Tango is connected. This generally doesn't happen but
+            // technically could because they happen in separate
+            // threads. Otherwise the link will be made in startTango().
+            if(cameraTextures_ != null && tango_ != null) {
+                if (tangoConnected_ && cameraTextures_.get(cameraId) != textureName)
+                    tango_.connectTextureId(cameraId, textureName);
+                cameraTextures_.put(cameraId, textureName);
+            }
+        }
+        else
+            cameraTextures_.remove(cameraId);
+    }
+
+    public synchronized void updateTexture(int cameraId) {
+        if (tangoConnected_) {
+            try {
+                tango_.updateTexture(cameraId);
+            }
+            catch (TangoInvalidException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Point getCameraFrameSize(int cameraId) {
+        // TangoCameraIntrinsics intrinsics = mTango.getCameraIntrinsics(cameraId);
+        // return new Point(intrinsics.width, intrinsics.height);
+        return new Point(640, 480);
+        //   return new Point(1280, 720);
+    }
+
+    private void startTango() {
+        try {
+
+            tangoConnected_ = true;
+            Log.i("startTango", "Tango Connected");
+
+            Display display = getWindowManager().getDefaultDisplay();
+            mDisplayRotation = display.getRotation();
+
+            // Attach cameras to textures.
+            synchronized(this) {
+                for (Map.Entry<Integer, Integer> entry : cameraTextures_.entrySet())
+                    tango_.connectTextureId(entry.getKey(), entry.getValue());
+            }
+
+            // Attach Tango listener.
+            ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<>();
+            framePairs.add(new TangoCoordinateFramePair(
+                    TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                    TangoPoseData.COORDINATE_FRAME_DEVICE));
+            tango_.connectListener(framePairs, new Tango.TangoUpdateCallback(){
+                @Override
+                public void onPointCloudAvailable(TangoPointCloudData pointCloud) {
+                    mPointCloudManager.updatePointCloud(pointCloud);
+                }
+
+                @Override
+                public void onPoseAvailable(TangoPoseData tangoPoseData) {
+                }
+
+                @Override
+                public void onXyzIjAvailable(TangoXyzIjData tangoXyzIjData) {
+                }
+                @Override
+                public void onTangoEvent(TangoEvent tangoEvent) {
+                    //Log.i("TangoEvent", String.format("%s: %s", tangoEvent.eventKey, tangoEvent.eventValue));
+                }
+                @Override
+                public void onFrameAvailable(int i) {
+                    //Log.i("onFrameAvailabe", "Main onFrameAvailabe called");
+                    if (i == TangoCameraIntrinsics.TANGO_CAMERA_COLOR) {
+                        // mColorCameraPreview.onFrameAvailable();
+                        view_.requestRender();
+                        if(renderer_.argbInt != null){
+
+                        }
+                    }
+                }
+            });
+
+            tango_.experimentalConnectOnFrameListener(TangoCameraIntrinsics.TANGO_CAMERA_COLOR,
+                    new Tango.OnFrameAvailableListener() {
+                        @Override
+                        public void onFrameAvailable(TangoImageBuffer tangoImageBuffer, int i) {
+                            mCurrentImageBuffer = copyImageBuffer(tangoImageBuffer);
+                            // Log.i("onFrame",String.format("Tango Image Size: %dx%d",
+                            //     mCurrentImageBuffer.width,mCurrentImageBuffer.height));
+                        }
+
+                        TangoImageBuffer copyImageBuffer(TangoImageBuffer imageBuffer) {
+                            ByteBuffer clone = ByteBuffer.allocateDirect(imageBuffer.data.capacity());
+                            imageBuffer.data.rewind();
+                            clone.put(imageBuffer.data);
+                            imageBuffer.data.rewind();
+                            clone.flip();
+                            return new TangoImageBuffer(imageBuffer.width, imageBuffer.height,
+                                    imageBuffer.stride, imageBuffer.frameNumber,
+                                    imageBuffer.timestamp, imageBuffer.format, clone,
+                                    imageBuffer.exposureDurationNs);
+                        }
+                    });
+        }
+        catch (TangoOutOfDateException e) {
+            Toast.makeText(
+                    this,
+                    "TangoCore update required",
+                    Toast.LENGTH_SHORT).show();
+        }
+        catch (TangoErrorException e) {
+            Toast.makeText(
+                    this,
+                    "Tango error: " + e.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private  void stopTango() {
+        try {
+            if (tangoConnected_) {
+                tango_.disconnect();
+                tangoConnected_ = false;
+            }
+        }
+        catch (TangoErrorException e) {
+            Toast.makeText(
+                    this,
+                    "Tango error: " + e.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public class RunDetection implements Runnable{
+        @Override
+        public void run(){
+            final int  sleepShort = 5;
+            while(true) {
+                try {
+                    if(tangoConnected_ == false){
+                        Thread.sleep(sleepShort);
+                        continue;
+                    }
+                    if (null != renderer_.argbInt) {
+
+                    } else {
+                        Thread.sleep(sleepShort);
+                    }
+                }catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+
+
+    public boolean getOrientationDir(PointF bbox_in){
+        boolean isClockwise = false;
+        float adjacent = 320.0f - 640.0f*bbox_in.x;
+        if(adjacent < 0.f){
+            isClockwise = true;
+        }
+        else{
+            isClockwise = false;
+        }
+        return isClockwise;
+    }
+
+    public double getOrientationVal(PointF bbox_in){
+        double orientation = 0;
+        double adjacent = (double)(480.0f - 480.0f*bbox_in.y);
+        double opposite = (double)(Math.abs((320.0f - 640.0f*bbox_in.x)));
+        orientation = Math.toDegrees(Math.atan(opposite/adjacent));
+        return orientation;
     }
 }
+
+
